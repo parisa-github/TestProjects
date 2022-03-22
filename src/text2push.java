d39fe571e0a34372adae8fe75c663945d7ab5882
ParisaOmid <ParisaOmid@10.0.0.80>
2022-03-22 12:35:27 -0700

    second commit test2push.java

---
+++ src/text2push.java
@@ -0,0 +1,14 @@
+
+public class text2push {
+	
+	
+	public static void main(String[] args) {
+		
+		int num = 10;
+		int num1 = 20;
+		int total = num+num1;
+		System.out.println(total);
+				
+	}
+
+}
